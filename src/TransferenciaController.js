92c684ab0dcc630e420efd651380c8e3a6d293f7
Giovanna Garcia <ggarciabas@gmail.com>
2019-11-08 17:04:25 -0300

    Alterando uso de data

--- src/TransferenciaController.js
+++ src/TransferenciaController.js
@@ -44,7 +44,8 @@ module.exports = {
             }
             // t_atual.insc já foi subtraido na chamada anterior
             const turmas = await Turma.find({
-                data: { $gt: t_atual.data }
+                data: { $gt: t_atual.data },
+                datainscricao: { $gt: new Date()},
             },
                 [], {
                 sort: {
